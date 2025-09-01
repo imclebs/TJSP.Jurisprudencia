3a4b6f40b39bde7f4ec6adbb23c5ff0eda2815ec
imclebs <clebersonvictor2014@gmail.com>
2025-09-01 17:59:48 -0300

    Desafio Tec. - Crawler + API TJSP

--- TJSP.Api/obj/Debug/net8.0/TJSP.Api.AssemblyInfo.cs
+++ TJSP.Api/obj/Debug/net8.0/TJSP.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TJSP.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9b295986ac703b2fade634ee7b88e74e6adae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TJSP.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TJSP.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
